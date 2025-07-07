2f9388d4ee8c4bece85ee783a6e076ff7d48aad9
Atul Singh <atul.singh@delta6labs.com>
2025-07-07 21:23:38 +0530

    add proxy

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+app.set('trust proxy', 1);
 
 // Security middleware
 app.use(helmet({
